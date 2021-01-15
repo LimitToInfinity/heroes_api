659b7a65ffa644b503fcdf91e0ca5e88b9591ff8
LimitToInfinity <21stLimitless@gmail.com>
2021-01-15 12:42:22 -0700

    Adds Hero seeds and index action.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Hero.destroy_all
+
+Hero.create(
+  [
+    {
+      name: 'Iron Man',
+      image: 'https://i.pinimg.com/originals/2e/8a/41/2e8a4149d974c46e2da30589f03ecdf8.jpg'
+    }, {
+      name: 'Aquaman',
+      image: 'https://static.wikia.nocookie.net/marvel_dc/images/a/a0/Aquaman_Vol_8_48_Textless_Variant.jpg/revision/latest/top-crop/width/360/height/450?cb=20190519131903'
+    }, {
+      name: 'Wonder Woman',
+      image: 'https://static3.srcdn.com/wordpress/wp-content/uploads/2020/10/Wonder-Woman-Future-State.jpg'
+    }, {
+      name: 'Gamora',
+      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/08/Gamora-cover.jpg/250px-Gamora-cover.jpg'
+    }
+  ]
+)
